79329f37c85dc4520096366a682697852dda0090
kishor.sarkar <kishor.sarkar@inadev.com>
2025-05-11 21:21:11 +0530

    add

---
+++ index.js
@@ -0,0 +1,9 @@
+// api/index.js
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello from Express on Vercel!');
+});
+
+module.exports = app;
